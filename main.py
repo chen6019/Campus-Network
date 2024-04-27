9c8bec211c26ca2dac2b3dbe098cc7377a496062
chen6019 <89252350+chen6019@users.noreply.github.com>
2024-04-27 13:19:25 +0800

    更新登录功能，添加登陆成功自动退出功能，添加当日志文件超过20MB时自动删除日志文件内容

--- main.py
+++ main.py
@@ -11,6 +11,8 @@ import win32com.client
 import threading
 
 def on_closing():
+    with open(file_path, 'a') as f:
+            f.write(f"执行退出程序！\n\n\n\n")
     os._exit(0)
 def login():
     # 获取用户名和密码
@@ -25,13 +27,11 @@ def login():
         save_info()
 
     # 显示消息
-    login_info_label.config(text="正在登录，请稍后！\n速度取决于您的网络...")
+    login_info_label.config(text="正在登录，请稍后！\n速度取决于您的网络...", font=("微软雅黑", 16))
 
     # 创建一个新的线程来进行网络请求
     t = threading.Thread(target=do_request, args=(username, password))
     t.start()
-
-    # messagebox.showinfo("开始登陆！", "开始执行登陆操作！\n日志将保存在log.txt中！")
 
 def get_url(username, password):
     timestamp = int(time.time() * 1000)
@@ -65,15 +65,25 @@ def handle_response(response, error):
     timestamp = int(time.time() * 1000)
     # 检查返回值中是否包含"result":"ok"
     if b'"result":"ok"' in response:
-        root.after(0, lambda: login_info_label.config(text=f"登录成功{timestamp}", font=("微软雅黑", 20)))
+        if opt_out_var.get():
+            countdown(4)
+        else:
+            root.after(0, lambda: login_info_label.config(text=f"登录成功{timestamp}", font=("微软雅黑", 22)))
     else:
-        root.after(0, lambda: login_info_label.config(text=f"登录失败{response}", font=("微软雅黑", 20)))
+        root.after(0, lambda: login_info_label.config(text=f"登录失败{response}", font=("微软雅黑", 16)))
+        
+def countdown(seconds):
+    if seconds > 0:
+        login_info_label.config(text=f"登录成功！\n即将在{seconds-1}秒后退出程序！", font=("微软雅黑", 17))
+        root.after(1000, countdown, seconds-1)  # 在1秒后再次调用countdown函数
+        if seconds < 2:
+            on_closing()
 
 def do_request(username, password):
     url = get_url(username, password)
     response, error = send_request(url)
     handle_response(response, error)
-# 例如，在你的save_info函数中，你应该这样写：
+
 def save_info():
     # 获取用户名和密码
     username = user_entry.get()
@@ -81,12 +91,17 @@ def save_info():
     timestamp = int(time.time() * 1000)
     # 获取复选框的状态
     auto_login = bool(auto_login_var.get())
+    global opt_out_var  # 声明 opt_out 是一个全局变量
+    opt_out = bool(opt_out_var.get())
+    global save_password_var  # 声明 save_password_var 是一个全局变量
+    save_password = bool(save_password_var.get())
+    
 
     # 保存用户名和密码
     with open(login_info_path, 'w') as f:
-        json.dump({'username': username, 'password': password, 'auto_login': auto_login}, f)
+        json.dump({'username': username, 'password': password,  'opt_out': opt_out, 'save_password': save_password,'auto_login': auto_login}, f)
     # 显示消息框
-    login_info_label.config(text=f"保存账号密码成功{timestamp}")
+    login_info_label.config(text=f"保存账号密码成功{timestamp}", font=("微软雅黑", 16))
 
 def set_auto_start():
     # 获取当前exe程序的绝对路径
@@ -188,6 +203,13 @@ def check_task_exists(task_name):
             return True
     return False
 
+def truncate_large_file(file_path, max_size=1024*1024*20):
+    # 检查文件大小
+    if os.path.getsize(file_path) > max_size:
+        # 文件大小超过限制
+        with open(file_path, 'w') as f:
+            f.truncate(0)  # 将文件截断到0字节
+
 # 创建主窗口
 root = tk.Tk()
 root.protocol("WM_DELETE_WINDOW", on_closing)
@@ -214,6 +236,8 @@ if not os.path.exists(data_dir):
 file_path = os.path.join(data_dir, 'log.txt')
 login_info_path = os.path.join(data_dir, 'login_info.json')
 
+truncate_large_file(file_path)
+
 # 创建一个标签控件显示操作提示，并设置文本大小为15
 label_label = tk.Label(root, text="不重启和断网线\n就不会掉线(除了服务器原因)", font=("微软雅黑", 14))
 label_label.place(x=55, y=5)
@@ -237,6 +261,10 @@ password_label.place(x=25, y=115)
 password_entry = tk.Entry(root, font=("微软雅黑", 14), show="*")
 password_entry.place(x=125, y=115)
 
+# 创建一个复选框控件作为自动登录的选项
+opt_out_var = tk.IntVar()
+opt_out_checkbutton = tk.Checkbutton(root, text="自动退出", variable=opt_out_var)
+opt_out_checkbutton.place(x=15, y=145)
 # 创建一个复选框控件作为保存密码的选项
 save_password_var = tk.IntVar()
 save_password_checkbutton = tk.Checkbutton(root, text="保存密码", variable=save_password_var)
@@ -285,6 +313,8 @@ if os.path.exists(login_info_path):
         user_entry.insert(0, login_info['username'])  # 将文件中的用户名更新到用户输入框
         password_entry.insert(0, login_info['password'])  # 将文件中的密码更新到密码输入框
         auto_login_var.set(bool(login_info.get('auto_login', 0)))  # 设置自动登录选项为文件中的值（如果不存在则默认为0）
+        opt_out_var.set(bool(login_info.get('opt_out', 0))) # 设置自动退出选项为文件中的值（如果不存在则默认为0）
+        save_password_var.set(bool(login_info.get('save_password', 0))) # 设置自动退出选项为文件中的值（如果不存在则默认为0）
 # 如果自动登录选项被选中，则执行登录函数
 if bool(auto_login_var.get()):
     login()
